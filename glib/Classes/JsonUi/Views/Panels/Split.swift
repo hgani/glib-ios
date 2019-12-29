e04ea9bf87863264ae95a6f3d94a4680e084595a
Hendrik Gani <hendrik.gani@gmail.com>
2019-12-29 21:10:13 +1100

    Fixing split panel

--- glib/Classes/JsonUi/Views/Panels/Split.swift
+++ glib/Classes/JsonUi/Views/Panels/Split.swift
@@ -2,23 +2,38 @@ class JsonView_Panels_SplitV1: JsonView {
     private let panel = GSplitPanel().width(.matchParent)
 
     override func initView() -> UIView {
-//        GLog.d("======")
-//        GLog.d(spec.debugDescription)
-//        GLog.d("======")
-//        let content = spec["content"]
-        if let center = spec["centerViews"].presence {
+//        if let center = spec["centerViews"].presence {
+//            return panel.withViews(
+//                createSubview(spec["leftViews"], center: false),
+//                createSubview(center, center: true),
+//                createSubview(spec["rightViews"], center: false)
+//            )
+//        } else {
+//            return panel.withViews(
+//                left: createSubview(spec["leftViews"], center: false),
+//                right: createSubview(spec["rightViews"], center: false)
+//            )
+//        }
+
+        if let center = spec["center"].presence {
             return panel.withViews(
-                createSubview(spec["leftViews"], center: false),
+                createSubview(spec["left"], center: false),
                 createSubview(center, center: true),
-                createSubview(spec["rightViews"], center: false)
+                createSubview(spec["right"], center: false)
             )
         } else {
             return panel.withViews(
-                left: createSubview(spec["leftViews"], center: false),
-                right: createSubview(spec["rightViews"], center: false)
+                left: createSubview(spec["left"], center: false),
+                right: createSubview(spec["right"], center: false)
             )
         }
     }
+
+//    private static func initVerticalPanel(_ panel: GVerticalPanel, spec: Json, screen: GScreen) {
+//        if let wrapper = JsonView_Panels_VerticalV1(spec, screen).createView() as? GVerticalPanel {
+//            panel.addView(wrapper.width(.matchParent))
+//        }
+//    }
 
     private func createSubview(_ subviewSpec: Json, center: Bool) -> UIView {
         if subviewSpec.isNull {
@@ -33,18 +48,22 @@ class JsonView_Panels_SplitV1: JsonView {
 //        }
 //        return view
 
-        let panel = GVerticalPanel()
-        let childViews = subviewSpec.arrayValue
-        let subviews: [UIView] = childViews.compactMap { viewSpec -> UIView? in
-            if let jsonView = JsonView.create(spec: viewSpec, screen: screen) {
-                return jsonView.createView()
-            }
-            return nil
-        }
+//        let panel = GVerticalPanel()
+//        let childViews = subviewSpec.arrayValue
+//        let subviews: [UIView] = childViews.compactMap { viewSpec -> UIView? in
+//            if let jsonView = JsonView.create(spec: viewSpec, screen: screen) {
+//                return jsonView.createView()
+//            }
+//            return nil
+//        }
+//
+//        for view in subviews {
+//            panel.addView(view)
+//        }
+//
+//        return panel
 
-        for view in subviews {
-            panel.addView(view)
-        }
+        let panel = JsonViewDefaultPanel.createPanel(spec: subviewSpec, screen: screen)
 
         return panel
     }
