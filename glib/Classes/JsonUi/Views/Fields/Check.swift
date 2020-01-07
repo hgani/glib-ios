94405c9ca6aa5b721b28f6e137262d5903767681
Hendrik Gani <hendrik.gani@gmail.com>
2020-01-07 19:10:14 +1100

    Refactor checkbox

--- glib/Classes/JsonUi/Views/Fields/Check.swift
+++ glib/Classes/JsonUi/Views/Fields/Check.swift
@@ -1,9 +1,8 @@
-
 #if INCLUDE_MDLIBS
 
 import MBCheckboxButton
 
-class JsonView_Fields_CheckV1: JsonView, SubmittableField {
+class JsonView_Fields_CheckV1: JsonView_AbstractField, SubmittableField {
     private let checkbox = MCheckBox()
 
     var name: String?
@@ -23,51 +22,6 @@ class JsonView_Fields_CheckV1: JsonView, SubmittableField {
 
     func validate() -> Bool {
         return true
-    }
-}
-
-class MCheckBox: CheckboxButton {
-    fileprivate var helper: ViewHelper!
-
-    init() {
-        super.init(frame: .zero)
-        initialize()
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        initialize()
-    }
-
-    private func initialize() {
-        helper = ViewHelper(self)
-    }
-
-    public func width(_ width: Int) -> Self {
-        helper.width(width)
-        return self
-    }
-
-    public func width(_ width: LayoutSize) -> Self {
-        helper.width(width)
-        return self
-    }
-
-    public func height(_ height: Int) -> Self {
-        helper.height(height)
-        return self
-    }
-
-    public func height(_ height: LayoutSize) -> Self {
-        helper.height(height)
-        return self
-    }
-
-    func title(_ str: String) -> Self {
-        setTitle(str, for: [])
-        setTitleColor(.black, for: [])
-        titleLabel?.font = RobotoFonts.Style.regular.font
-        return self
     }
 }
 
---
+++ glib/Classes/Material/MCheckBox.swift
@@ -0,0 +1,50 @@
+#if INCLUDE_MDLIBS
+
+import MBCheckboxButton
+
+class MCheckBox: CheckboxButton {
+    fileprivate var helper: ViewHelper!
+
+    init() {
+        super.init(frame: .zero)
+        initialize()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialize()
+    }
+
+    private func initialize() {
+        helper = ViewHelper(self)
+    }
+
+    public func width(_ width: Int) -> Self {
+        helper.width(width)
+        return self
+    }
+
+    public func width(_ width: LayoutSize) -> Self {
+        helper.width(width)
+        return self
+    }
+
+    public func height(_ height: Int) -> Self {
+        helper.height(height)
+        return self
+    }
+
+    public func height(_ height: LayoutSize) -> Self {
+        helper.height(height)
+        return self
+    }
+
+    func title(_ str: String) -> Self {
+        setTitle(str, for: [])
+        setTitleColor(.black, for: [])
+        titleLabel?.font = RobotoFonts.Style.regular.font
+        return self
+    }
+}
+
+#endif
