645a0287219cff224221f909ad7b554a1ad67a21
apinrdw <apinrdw@gmail.com>
2020-03-10 14:52:40 +0700

    restrict only digits on NumberField

--- glib/Classes/JsonUi/Views/Fields/Number.swift
+++ glib/Classes/JsonUi/Views/Fields/Number.swift
@@ -1,5 +1,20 @@
 class JsonView_Fields_NumberV1: JsonView_AbstractTextV1 {
+    private var delegate: Delegate?
+    
     override func initView() -> UITextField {
-        return super.initTextField().keyboardType(.numberPad)
+        delegate = Delegate()
+        
+        let numberField = super.initTextField()
+        numberField.delegate = delegate
+        return numberField.keyboardType(.numberPad)
+    }
+    
+    class Delegate: NSObject, UITextFieldDelegate {
+        func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool
+        {
+            let allowedCharacters = CharacterSet.decimalDigits
+            let characterSet = CharacterSet(charactersIn: string)
+            return allowedCharacters.isSuperset(of: characterSet)
+        }
     }
 }
