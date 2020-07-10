8f19393e47293e8aa9ab471ca68bd944d8f730ce
apinrdw <apinrdw@gmail.com>
2020-07-10 16:25:27 +0700

    tweak line chart, added column chart

---
+++ glib/Classes/JsonUi/Views/Charts/ChartColumn.swift
@@ -0,0 +1,112 @@
+import Charts
+
+class JsonView_Charts_ColumnV1: JsonView {
+    private var chartView = MBarChartView()
+    
+    override func initView() -> UIView {        
+        chartView.width(.matchParent).height(200).fitBars(true).fitScreen()
+        
+        if let stacked = spec["stacked"].bool, stacked == true {
+            var xEntries = [Double]()
+            var yEntries = [[Double]]()
+            var titles = [String]()
+            var barColors = [UIColor]()
+            
+            for (dataIndex, data) in spec["dataGroups"].arrayValue.enumerated() {
+                titles.append(data["title"].stringValue)
+                barColors.append(UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0))
+                
+                for point in data["points"].dictionaryValue {
+                    if let index = xEntries.firstIndex(of: Double(point.key)!) {
+                        yEntries[index].append(Double(point.value.intValue))
+                    } else {
+                        xEntries.append(Double(point.key)!)
+                        yEntries.append([Double(point.value.intValue)])
+                    }
+                }
+            }
+            
+            let yVals = xEntries.enumerated().map { (index, xEntry) -> BarChartDataEntry in
+                return BarChartDataEntry(x: xEntry, yValues: yEntries[index])
+            }
+            let set = BarChartDataSet(values: yVals, label: "")
+            set.drawIconsEnabled = false
+            set.colors = barColors
+            set.stackLabels = titles
+            
+            let data = BarChartData(dataSet: set)
+            chartView.data(data)
+        }
+        else {
+            var sets = [BarChartDataSet]()
+            for (dataIndex, data) in spec["dataGroups"].arrayValue.enumerated() {
+                var values = [BarChartDataEntry]()
+                for point in data["points"].dictionaryValue {
+                    values.append(BarChartDataEntry(x: Double(point.key)!, y: Double(point.value.intValue)))
+                }
+                
+                let set = BarChartDataSet(values: values, label: data["title"].stringValue)
+                set.setColor(UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0))
+                sets.append(set)
+            }
+            
+            let data = BarChartData(dataSets: sets)
+            data.barWidth = 0.25
+            data.groupBars(fromX: sets[0].xMin, groupSpace: 0.1, barSpace: 0.05)
+            
+            chartView.xAxis.axisMinimum = sets[0].xMin
+            chartView.xAxis.axisMaximum = sets[0].xMin + data.groupWidth(groupSpace: 0.1, barSpace: 0.05) * Double(sets.count)
+            chartView.data = data
+        }
+        
+        return chartView
+    }
+}
+
+class MBarChartView: BarChartView {
+    fileprivate var helper: ViewHelper!
+    
+    init() {
+        super.init(frame: .zero)
+        initialize()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(frame: .zero)
+        initialize()
+    }
+    
+    private func initialize() {
+        helper = ViewHelper(self)
+    }
+    
+    public func width(_ width: Int) -> Self {
+        helper.width(width)
+        return self
+    }
+    
+    public func width(_ width: LayoutSize) -> Self {
+        helper.width(width)
+        return self
+    }
+    
+    public func height(_ height: Int) -> Self {
+        helper.height(height)
+        return self
+    }
+    
+    public func height(_ height: LayoutSize) -> Self {
+        helper.height(height)
+        return self
+    }
+    
+    public func data(_ data: BarChartData) -> Self {
+        self.data = data
+        return self
+    }
+    
+    public func fitBars(_ value: Bool) -> Self {
+        self.fitBars = value
+        return self
+    }
+}
--- glib/Classes/JsonUi/Views/Charts/Line.swift
+++ glib/Classes/JsonUi/Views/Charts/Line.swift
@@ -4,85 +4,104 @@ import Charts
 
 class JsonView_Charts_LineV1: JsonView {
     private var chartView = MLineChartView()
-
+    private let data = LineChartData(dataSets: [])
+    
     override func initView() -> UIView {
-        let data = LineChartData(dataSets: [])
-        var xValues = [String]()
-        for series in spec["dataSeries"].arrayValue {
-            var entries = [ChartDataEntry]()
-            var index = 0
-//            for (key, value) in series["dataPoints"].dictionaryValue {
-//                // TODO: json value didn't loop in order
-//                GLog.d("\(key) \(value.doubleValue)")
-//                entries.append(ChartDataEntry(x: Double(index), y: value.doubleValue))
-//                index = index + 1
-//            }
-            for point in series["dataPoints"].arrayValue {
-                GLog.d(point["x"].stringValue)
-                if !xValues.contains(point["x"].stringValue) {
-                    xValues.append(point["x"].stringValue)
-                }
-                entries.append(ChartDataEntry(x: Double(index), y: point["y"].doubleValue))
-                index += 1
-            }
-            let color = UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
-            let set = LineChartDataSet(values: entries, label: series["title"].stringValue)
-            set.mode = .cubicBezier
-            set.setColor(color)
-            set.setCircleColor(color)
-
-            data.addDataSet(set)
-        }
-
-        GLog.d(xValues.description)
-        let chartFormatter = DateValueFormatter(labels: xValues)
+        renderData(spec["dataSeries"].presence)
+        fetchData(spec["nextPage"].presence)
+        
+        let chartFormatter = DateValueFormatter()
         let xAxis = XAxis()
         xAxis.valueFormatter = chartFormatter
-
         chartView.xAxis.valueFormatter = xAxis.valueFormatter
-        chartView.width(.matchParent).height(300).data(data)
-
+        chartView.width(.matchParent).height(200).data(data).fitScreen()
+        
         return chartView
+    }
+    
+    private func renderData(_ specDataSeries: Json?) {
+        if let dataSeries = specDataSeries?.array {
+            for series in dataSeries {
+                var entries = [ChartDataEntry]()
+                for point in series["points"].arrayValue {
+                    entries.append(ChartDataEntry(x: timestamp(point["x"].stringValue),
+                                                  y: point["y"].doubleValue))
+                }
+                let color = UIColor(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1.0)
+                let set = LineChartDataSet(values: entries, label: series["title"].stringValue)
+                set.mode = .cubicBezier
+                set.setColor(color)
+                set.setCircleColor(color)
+                data.addDataSet(set)
+            }
+            chartView.notifyDataSetChanged()
+        }
+    }
+    
+    private func fetchData(_ nextPage: Json?) {
+        if let url = nextPage?["url"].string {
+            Rest.get(url: url).execute { (response) -> Bool in
+                self.renderData(response.content["dataSeries"].presence)
+                self.fetchData(response.content["nextPage"].presence)
+                return true
+            }
+        }
+    }
+    
+    private func timestamp(_ dateStr: String) -> Double {
+        let dateFormatterGet = DateFormatter()
+        
+        dateFormatterGet.dateFormat = "yyyy-MM-dd"
+        if let date = try dateFormatterGet.date(from: dateStr) {
+            return Double(date.timeIntervalSince1970)
+        }
+        
+        dateFormatterGet.dateFormat = "EEE, dd MMM yyyy"
+        if let date = try dateFormatterGet.date(from: dateStr) {
+            return Double(date.timeIntervalSince1970)
+        }
+        
+        return Double(0)
     }
 }
 
 class MLineChartView: LineChartView {
     fileprivate var helper: ViewHelper!
-
+    
     init() {
         super.init(frame: .zero)
         initialize()
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(frame: .zero)
         initialize()
     }
-
+    
     private func initialize() {
         helper = ViewHelper(self)
     }
-
+    
     public func width(_ width: Int) -> Self {
         helper.width(width)
         return self
     }
-
+    
     public func width(_ width: LayoutSize) -> Self {
         helper.width(width)
         return self
     }
-
+    
     public func height(_ height: Int) -> Self {
         helper.height(height)
         return self
     }
-
+    
     public func height(_ height: LayoutSize) -> Self {
         helper.height(height)
         return self
     }
-
+    
     public func data(_ data: LineChartData) -> Self {
         self.data = data
         return self
@@ -92,21 +111,11 @@ class MLineChartView: LineChartView {
 public class DateValueFormatter: NSObject, IAxisValueFormatter {
     private let dateFormatterGet = DateFormatter()
     private let dateFormatterPrint = DateFormatter()
-    var labels: [String] = []
-
-    init(labels: [String]) {
-        super.init()
-        dateFormatterGet.dateFormat = "EEE, dd MMM yyyy"
-        dateFormatterPrint.dateFormat = "MMM dd"
-        self.labels = labels
-    }
-
+    
     public func stringForValue(_ value: Double, axis: AxisBase?) -> String {
-        if let date = dateFormatterGet.date(from: labels[Int(value)]) {
-            return dateFormatterPrint.string(from: date)
-        }
-
-        return value.description
+        let date = Date(timeIntervalSince1970: value)
+        dateFormatterPrint.dateFormat = "MMM dd"
+        return dateFormatterPrint.string(from: date)
     }
 }
 
