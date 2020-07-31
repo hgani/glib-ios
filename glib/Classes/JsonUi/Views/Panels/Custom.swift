47a5d90c617e481d53c5e2ab553aa3754b09c7a9
Hendrik Gani <hendrik.gani@gmail.com>
2020-07-31 11:09:23 +1000

    Fix custom template loading

--- glib/Classes/JsonUi/Views/Panels/Custom.swift
+++ glib/Classes/JsonUi/Views/Panels/Custom.swift
@@ -1,7 +1,9 @@
 class JsonView_Panels_CustomV1: JsonView {
     override func initView() -> UIView {
+        var data = spec["data"]
+        data["template"] = spec["template"]
 
-        if let template = JsonTemplate.create(spec: spec, screen: screen) {
+        if let template = JsonTemplate.create(spec: data, screen: screen) {
             return template.createPanel()
         }
 
