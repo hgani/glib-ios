41f0731362d74987d7dc1b1fc04ceed9338c41e3
Hendrik Gani <hendrik.gani@gmail.com>
2020-09-13 13:39:02 +1000

    Implement autoResizeForKeyboard

--- glib/Classes/JsonUi/Views/Panels/Scroll.swift
+++ glib/Classes/JsonUi/Views/Panels/Scroll.swift
@@ -2,6 +2,8 @@ class JsonView_Panels_Scroll: JsonView {
     private let panel = GScrollPanel()
 
     override func initView() -> UIView {
+        panel.autoResizeForKeyboard(screen: screen)
+
         JsonViewDefaultPanel.initPanel(panel.contentView, spec: spec, screen: screen)
 
         // NOTE: This only works when scrolling is activated, i.e. content is taller than the scroll panel
