afb7c5f4bb12de837223910d5212aac8e1c1970b
Hendrik Gani <hendrik.gani@gmail.com>
2021-05-14 19:01:47 +1000

    Generalize code

--- glib/Classes/JsonUi/Views/Fields/Date.swift
+++ glib/Classes/JsonUi/Views/Fields/Date.swift
@@ -10,19 +10,28 @@ class JsonView_Fields_Date: JsonView_AbstractDate {
 
     override func initView() -> UIView {
         textField = super.initTextField()
-        setInputViewDatePicker(field: textField, mode: .date, target: self, selector: #selector(tapDone))
+        setInputViewDatePicker(field: textField, mode: .date, onSelected: {
+            self.commitSelection()
+        })
 
         return textField
     }
 
-    @objc func tapDone() {
+    private func commitSelection() {
         if let datePicker = textField.inputView as? UIDatePicker {
             textField.text = dateFormatter.string(from: datePicker.date)
         }
         textField.resignFirstResponder()
     }
 
-    func setInputViewDatePicker(field: MTextField, mode: UIDatePicker.Mode, target: Any, selector: Selector) {
+//    @objc func tapDone() {
+//        if let datePicker = textField.inputView as? UIDatePicker {
+//            textField.text = dateFormatter.string(from: datePicker.date)
+//        }
+//        textField.resignFirstResponder()
+//    }
+
+    func setInputViewDatePicker(field: MTextField, mode: UIDatePicker.Mode, onSelected: @escaping () -> Void) {
         let screenWidth = UIScreen.main.bounds.width
         let datePicker = UIDatePicker(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 216))
         datePicker.datePickerMode = mode
@@ -30,14 +39,18 @@ class JsonView_Fields_Date: JsonView_AbstractDate {
 
         let toolBar = UIToolbar(frame: CGRect(x: 0.0, y: 0.0, width: screenWidth, height: 44.0))
         let flexible = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        let cancel = UIBarButtonItem(title: "Cancel", style: .plain, target: target, action: #selector(tapCancel))
-        let barButton = UIBarButtonItem(title: "Done", style: .plain, target: target, action: selector)
-        toolBar.setItems([cancel, flexible, barButton], animated: false)
+//        let cancel = UIBarButtonItem(title: "Cancel", style: .plain, target: target, action: #selector(tapCancel))
+        let cancel = GBarButtonItem().title("Cancel").onClick {
+            field.resignFirstResponder()
+        }
+        let done = GBarButtonItem().title("Done").onClick(onSelected)
+//        let barButton = UIBarButtonItem(title: "Done", style: .plain, target: target, action: selector)
+        toolBar.setItems([cancel, flexible, done], animated: false)
         field.inputAccessoryView = toolBar
     }
 
-    @objc func tapCancel() {
-        textField.resignFirstResponder()
-    }
+//    @objc func tapCancel() {
+//        textField.resignFirstResponder()
+//    }
 
 }
