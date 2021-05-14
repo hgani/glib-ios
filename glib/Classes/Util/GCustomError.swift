695526bdb321451f602ef66e60ccab54b9fa9c39
Hendrik Gani <hendrik.gani@gmail.com>
2021-05-14 11:23:58 +1000

    Properly implement localizedDescription

--- glib/Classes/Util/GCustomError.swift
+++ glib/Classes/Util/GCustomError.swift
@@ -1,6 +1,9 @@
 public struct GCustomError: Error {
     let message: String
+}
 
+// See https://stackoverflow.com/questions/44547486/custom-error-type-initializer
+extension GCustomError: LocalizedError {
     public var errorDescription: String? {
         return NSLocalizedString(message, comment: "")
     }
