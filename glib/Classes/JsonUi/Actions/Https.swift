2332a3cdb8e2afde2f0c07cff319f8afb8a99b39
Hendrik Gani <hendrik.gani@gmail.com>
2021-06-20 09:56:27 +1000

    Implement full http actions

--- glib/Classes/JsonUi/Actions/Https.swift
+++ glib/Classes/JsonUi/Actions/Https.swift
@@ -1,6 +1,9 @@
 class JsonAction_Http_Delete: JsonAction {
     override func silentExecute() -> Bool {
-        Rest.delete(url: spec["url"].stringValue).execute { response in
+        var params: GParams = spec["formData"].dictionaryObject ?? GParams()
+        params["authenticity_token"] = GAuth.csrfToken
+
+        Rest.delete(url: spec["url"].stringValue, params: params).execute { response in
             JsonAction.execute(spec: response.content["onResponse"], screen: self.screen, creator: self)
             return true
         }
@@ -11,12 +14,28 @@ class JsonAction_Http_Delete: JsonAction {
 
 class JsonAction_Http_Post: JsonAction {
     override func silentExecute() -> Bool {
-        let params: GParams = spec["formData"].dictionaryObject ?? GParams()
+        var params: GParams = spec["formData"].dictionaryObject ?? GParams()
+        params["authenticity_token"] = GAuth.csrfToken
+
         Rest.post(url: spec["url"].stringValue, params: params).execute { response in
             JsonAction.execute(spec: response.content["onResponse"], screen: self.screen, creator: self)
             return true
         }
         
+        return true
+    }
+}
+
+class JsonAction_Http_Patch: JsonAction {
+    override func silentExecute() -> Bool {
+        var params: GParams = spec["formData"].dictionaryObject ?? GParams()
+        params["authenticity_token"] = GAuth.csrfToken
+
+        Rest.patch(url: spec["url"].stringValue, params: params).execute { response in
+            JsonAction.execute(spec: response.content["onResponse"], screen: self.screen, creator: self)
+            return true
+        }
+
         return true
     }
 }
