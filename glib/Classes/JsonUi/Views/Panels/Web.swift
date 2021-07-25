662c615a1b85c434a067f85bb553d5e187c5676c
Hendrik Gani <hendrik.gani@gmail.com>
2021-07-25 11:45:10 +1000

    Add workaround for scroll top issue on pdf viewing

--- glib/Classes/JsonUi/Views/Panels/Web.swift
+++ glib/Classes/JsonUi/Views/Panels/Web.swift
@@ -1,6 +1,7 @@
 class JsonView_Panels_Web: JsonView {
     override func initView() -> UIView {
         let url = spec["url"].stringValue
-        return GWebView().load(url: url)
+        // Don't show indicator because this messes up top position when viewing a pdf file.
+        return GWebView().load(url: url, indicator: false)
     }
 }
--- glib/Classes/View/GWebView.swift
+++ glib/Classes/View/GWebView.swift
@@ -32,50 +32,20 @@ open class GWebView: WKWebView {
         super.didMoveToSuperview()
         helper.didMoveToSuperview()
     }
-//
-//    public func color(bg: UIColor?) -> Self {
-//        if let bgColor = bg {
-//            backgroundColor = bgColor
-//        }
-//        return self
-//    }
 
-//    public func width(_ width: Int) -> Self {
-//        helper.width(width)
-//        return self
-//    }
-//
-//    public func width(_ width: LayoutSize) -> Self {
-//        helper.width(width)
-//        return self
-//    }
-//
-//    public func width(weight: Float) -> Self {
-//        helper.width(weight: weight)
-//        return self
-//    }
-//
-//    public func height(_ height: Int) -> Self {
-//        helper.height(height)
-//        return self
-//    }
-//
-//    public func height(_ height: LayoutSize) -> Self {
-//        helper.height(height)
-//        return self
-//    }
-
-    public func load(url: URL) -> Self {
+    public func load(url: URL, indicator: Bool = true) -> Self {
         requestUrl = url
 
         GLog.i("Loading \(url) ...")
-        refresher.show()
+        if indicator {
+            refresher.show()
+        }
         load(URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 30))
         return self
     }
 
-    public func load(url: String) -> Self {
-        return load(url: URL(string: url)!)
+    public func load(url: String, indicator: Bool = true) -> Self {
+        return load(url: URL(string: url)!, indicator: indicator)
     }
 
     // Not meant to be used in conjuction with Http and Rest because sessions are not shared
@@ -89,10 +59,6 @@ open class GWebView: WKWebView {
             _ = load(url: url)
         }
     }
-//
-//    public func end() {
-//        // Ends chaining
-//    }
 }
 
 extension GWebView: WKNavigationDelegate {
