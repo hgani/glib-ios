23eaab3c4c94eb6da46d7ebb5417ed83cb25ceeb
Hendrik Gani <hendrik.gani@gmail.com>
2021-07-31 13:43:51 +1000

    Implement basic textarea

--- glib/Classes/JsonUi/Views/Fields/Textarea.swift
+++ glib/Classes/JsonUi/Views/Fields/Textarea.swift
@@ -1,98 +1,158 @@
 #if INCLUDE_MDLIBS
 
 class JsonView_Fields_Textarea: JsonView_AbstractField, SubmittableField {
-    private var delegate: Delegate?
-    private let view = MTextArea()
+//    private let view = MTextArea2()
+    private var view: MTextArea2!
 
     var name: String?
     var value: String {
-        return view.textView?.text ?? ""
+        return view.text ?? ""
     }
 
     override func initView() -> UIView {
+        view = MTextArea2(outlined: spec["styleClasses"].arrayValue.contains("outlined"))
+
         name = spec["name"].string
 
-        view.width(.matchParent)
-            .placeholder(spec["label"].stringValue)
-            .text(spec["value"].stringValue)
-            .maxLength(spec["maxLength"].uIntValue)
-            .styleClasses(spec["styleClasses"].arrayValue)
-        
-        delegate = Delegate(self)
-        view.textView?.delegate = delegate
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(updateJsonLogic), name: UITextView.textDidChangeNotification, object: nil)
-        
-//        self.registerToClosestForm(field: view)
+        view.labelView.text = spec["label"].string
+//        view.placeholder = spec["placeholder"].string
+//        view.text = spec["value"].string
+        view.addTarget(self, action: #selector(updateJsonLogic), for: .editingChanged)
 
+        view
+            .placeholder(spec["placeholder"].stringValue)
+            .text(spec["value"].stringValue)
+            .readOnly(spec["readOnly"].boolValue)
+
+//        name = spec["name"].string
+        //
+        //        view.width(.matchParent)
+        //            .placeholder(spec["label"].stringValue)
+        //            .text(spec["value"].stringValue)
+        //            .maxLength(spec["maxLength"].uIntValue)
+        //            .styleClasses(spec["styleClasses"].arrayValue)
+        //
+        //        delegate = Delegate(self)
+        //        view.textView?.delegate = delegate
+        //
+        //        NotificationCenter.default.addObserver(self, selector: #selector(updateJsonLogic), name: UITextView.textDidChangeNotification, object: nil)
+        //
+        ////        self.registerToClosestForm(field: view)
+        //
+
+//        let field = super.initTextField()
+//        return field
         return view
     }
-    
+
     @objc func updateJsonLogic() {
-        if let fieldName = spec["name"].string,
-            let form = closest(JsonView_Panels_Form.FormPanel.self, from: view),
-            let value = view.text {
+        if let fieldName = spec["name"].string, let form = closest(JsonView_Panels_Form.FormPanel.self, from: view) {
             updateFormData(form, fieldName, value)
         }
     }
 
     func validate() -> Bool {
-        if let textView = view.textView {
-            textView.becomeFirstResponder()
-            return textView.resignFirstResponder()
-        }
+//        if let textView = view.textView {
+//            textView.becomeFirstResponder()
+//            return textView.resignFirstResponder()
+//        }
         return true
     }
-    
-    func errors(_ text: String?) {
-        #if INCLUDE_MDLIBS
-        
-        view.errors(text)
-        
-        #endif
-    }
-    
-    class Delegate: NSObject, UITextViewDelegate {
-        private var field: JsonView_Fields_Textarea
-        
-        init(_ field: JsonView_Fields_Textarea) {
-            self.field = field
-        }
-        
-        @objc func updateJsonLogic() {
-            do {
-                if let fieldName = self.field.spec["name"].string {
-                    try Generic.sharedInstance.formData.value.merge(with: Json(parseJSON:
-                        """
-                        { "\(fieldName)" : "\(self.field.value)" }
-                        """
-                    ))
-                }
-            } catch {
-                GLog.d("Invalid json")
-            }
-        }
-        
-        func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
-            field.errors(nil)
-
-            if let validation = field.spec["validation"].presence {
-                if let required = validation["required"].presence {
-                    if UInt(textView.text.count) <= 0 {
-                        field.errors(required["message"].stringValue)
-                    }
-                }
-            }
-            
-            if let text = textView.text {
-                if let maxLength = field.spec["maxLength"].presence, maxLength.uIntValue > 0, UInt(text.count) > maxLength.uIntValue {
-                    field.errors("Maximum \(field.spec["maxLength"].uIntValue) characters")
-                }
-            }
-            
-            return true
-        }
-    }
 }
+
+//class JsonView_Fields_Textarea: JsonView_AbstractField, SubmittableField {
+//    private var delegate: Delegate?
+//    private let view = MTextArea()
+//
+//    var name: String?
+//    var value: String {
+//        return view.textView?.text ?? ""
+//    }
+//
+//    override func initView() -> UIView {
+//        name = spec["name"].string
+//
+//        view.width(.matchParent)
+//            .placeholder(spec["label"].stringValue)
+//            .text(spec["value"].stringValue)
+//            .maxLength(spec["maxLength"].uIntValue)
+//            .styleClasses(spec["styleClasses"].arrayValue)
+//
+//        delegate = Delegate(self)
+//        view.textView?.delegate = delegate
+//
+//        NotificationCenter.default.addObserver(self, selector: #selector(updateJsonLogic), name: UITextView.textDidChangeNotification, object: nil)
+//
+////        self.registerToClosestForm(field: view)
+//
+//        return view
+//    }
+//
+//    @objc func updateJsonLogic() {
+//        if let fieldName = spec["name"].string,
+//            let form = closest(JsonView_Panels_Form.FormPanel.self, from: view),
+//            let value = view.text {
+//            updateFormData(form, fieldName, value)
+//        }
+//    }
+//
+//    func validate() -> Bool {
+//        if let textView = view.textView {
+//            textView.becomeFirstResponder()
+//            return textView.resignFirstResponder()
+//        }
+//        return true
+//    }
+//
+//    func errors(_ text: String?) {
+//        #if INCLUDE_MDLIBS
+//
+//        view.errors(text)
+//
+//        #endif
+//    }
+//
+//    class Delegate: NSObject, UITextViewDelegate {
+//        private var field: JsonView_Fields_Textarea
+//
+//        init(_ field: JsonView_Fields_Textarea) {
+//            self.field = field
+//        }
+//
+//        @objc func updateJsonLogic() {
+//            do {
+//                if let fieldName = self.field.spec["name"].string {
+//                    try Generic.sharedInstance.formData.value.merge(with: Json(parseJSON:
+//                        """
+//                        { "\(fieldName)" : "\(self.field.value)" }
+//                        """
+//                    ))
+//                }
+//            } catch {
+//                GLog.d("Invalid json")
+//            }
+//        }
+//
+//        func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
+//            field.errors(nil)
+//
+//            if let validation = field.spec["validation"].presence {
+//                if let required = validation["required"].presence {
+//                    if UInt(textView.text.count) <= 0 {
+//                        field.errors(required["message"].stringValue)
+//                    }
+//                }
+//            }
+//
+//            if let text = textView.text {
+//                if let maxLength = field.spec["maxLength"].presence, maxLength.uIntValue > 0, UInt(text.count) > maxLength.uIntValue {
+//                    field.errors("Maximum \(field.spec["maxLength"].uIntValue) characters")
+//                }
+//            }
+//
+//            return true
+//        }
+//    }
+//}
 
 #endif
